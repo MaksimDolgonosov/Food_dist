2822eca28433905ee6d67ebe31b585576aeef2f8
max_air <max_air@bk.ru>
2022-01-21 16:47:39 +0300

    Калькулятор 2 закончен

--- js/script.js
+++ js/script.js
@@ -467,15 +467,44 @@ dots.forEach((dot, i) => {
 });
 
 // Калькулятор каллорий
-let sex="male", 
-height, weight, age, 
-active=;
+
+let sex, height, weight, age, active;
+if (localStorage.getItem("sex")) {
+    sex = localStorage.getItem("sex");
+
+} else {
+    sex = "female";
+    localStorage.setItem("sex", "female");
+}
+if (localStorage.getItem("data-active")) {
+    active = localStorage.getItem("data-active");
+} else {
+    active = 1.375;
+    localStorage.setItem("data-active", 1.375);
+}
+firstLoad();
+function firstLoad() {
+    document.querySelectorAll(`#gender div`).forEach(e => {
+        e.classList.remove("calculating__choose-item_active");
+        if (e.getAttribute("id") === localStorage.getItem("sex")) {
+            e.classList.add("calculating__choose-item_active");
+        }
+    });
+    document.querySelector(`#${sex}`).classList.add("calculating__choose-item_active");
+    document.querySelectorAll(`.calculating__choose_big div`).forEach(e => {
+        e.classList.remove("calculating__choose-item_active");
+        if (e.getAttribute("data-active") === localStorage.getItem("data-active")) {
+            e.classList.add("calculating__choose-item_active");
+        }
+    });
+
+
+}
+
+
+
 
 const result = document.querySelector(".calculating__result span");
-
-
-// let resultMale = (88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * active;
-// let sesultFemale = (447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * active;
 
 calcTotal();
 
@@ -500,15 +529,15 @@ function getStaticInformaton(parentElement, activeClass) {
         btn.addEventListener("click", e => {
             if (e.target.getAttribute("data-active")) {
                 active = e.target.getAttribute("data-active");
-
-                input.forEach(el => el.classList.remove("calculating__choose-item_active"));
-                e.target.classList.add("calculating__choose-item_active");
+                input.forEach(el => el.classList.remove(activeClass));
+                e.target.classList.add(activeClass);
+                localStorage.setItem("data-active", e.target.getAttribute("data-active"));
                 calcTotal();
             } else {
                 sex = e.target.getAttribute("id");
-
-                input.forEach(el => el.classList.remove("calculating__choose-item_active"));
-                e.target.classList.add("calculating__choose-item_active");
+                input.forEach(el => el.classList.remove(activeClass));
+                e.target.classList.add(activeClass);
+                localStorage.setItem("sex", e.target.getAttribute("id"));
                 calcTotal();
             }
         });
@@ -518,17 +547,24 @@ getStaticInformaton("#gender", "calculating__choose-item_active");
 getStaticInformaton(".calculating__choose_big", "calculating__choose-item_active");
 
 function getDinamycInformation(selector) {
+
     let input = document.querySelector(selector);
+
     input.addEventListener("input", () => {
+        if (input.value.match(/\D/g)) {
+            input.style.border = "1px solid red";
+        } else {
+            input.style.border = "none";
+        }
         switch (input.getAttribute("id")) {
             case "height":
-                height = input.value;
+                height = +input.value;
                 break;
             case "weight":
-                weight = input.value;
+                weight = +input.value;
                 break;
             case "age":
-                age = input.value;
+                age = +input.value;
                 break;
         }
         calcTotal();
